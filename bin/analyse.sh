79a9d9110001874249df063845c91e0991645355
thaddeus002 <thaddeus.dupont@free.fr>
2016-04-19 01:22:23 +0200

    added shell script to process projects analysis

---
+++ bin/analyse.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# Process the periodic analysis of declared projects
+
+
+#YANNKINS_HOME="/var/yannkins"
+#YANNKINS_HOME="."
+PROJECTS_HOME="${YANNKINS_HOME}/projets"
+SVN_HOME="${YANNKINS_HOME}/svn"
+
+
+#parcourt des projets
+for p in ${PROJECTS_HOME}/*; do
+    if [ $p != "${PROJECTS_HOME}/*" ]; then
+
+        . $p
+
+        if [ ${PROJECT_NAME}_ == _ ]; then
+            printf "File %s : Project name missing\n" "$p"
+            exit 1
+        fi
+
+        printf "Analysis of project %s\n" "${PROJECT_NAME}"
+
+        # Checkout of the project
+        printf "Checkout of repository\n"
+        if [ ${SVN_DEPOT}_ == _ ]; then
+            printf "Repository not specified\n"
+            exit 1
+        fi
+        cd ${SVN_HOME}
+        rm -rf ${PROJECT_NAME}
+        COMMANDE="svn co ${SVN_DEPOT}/trunk ${PROJECT_NAME}"
+        printf "%s\n" "${COMMANDE}"
+        ${YANNKINS_HOME}/bin/tache.sh "SVN_CHECKOUT_${PROJECT_NAME}" "${COMMANDE}"
+        cd ${YANNKINS_HOME}
+
+        # Compiling project
+        printf "Compilation\n"
+        COMP=$(echo -n ${COMPIL} | wc -c)
+        #if [ ${COMPIL}_ != _ ]; then
+        if [ ${COMP} -gt 0 ]; then 
+            cd ${SVN_HOME}/${PROJECT_NAME}
+            ${YANNKINS_HOME}/bin/tache.sh COMPILATION_${PROJECT_NAME} "${COMPIL}"
+            cd -
+        fi
+
+        # tests
+        printf "Tests\n"
+        TESTS=$(echo -n ${TESTS_UNI} | wc -c)
+        #if [ ${TESTS_UNI}_ != _ ]; then
+        if [ ${TESTS} -gt 0 ]; then 
+            cd ${SVN_HOME}/${PROJECT_NAME}
+            ${YANNKINS_HOME}/bin/tache.sh TESTS_${PROJECT_NAME} "${TESTS_UNI}"
+            cd -
+        fi
+
+        # Logs SVN
+        printf "Checking SVN logs\n"
+        #printf "#;auteur;date;nb lignes;commentaires\n" > svnlogl10.csv
+	printf "#;author;date;number of lines;commentaries\n" > svnlogl10.csv
+        svn log -l 10 ${SVN_DEPOT}/trunk | tr "|" ";" | tr "\n" "$" | sed -e 's/\$\$/; /g' | sed -e 's/\$--/\n--/g' | sed -re 's/-+\$//g' | sed -e 's/\$/\<br\/\>/g' >> svnlogl10.csv
+
+		printf "Creating project's page\n"
+		mv svnlogl10.csv ${YANNKINS_HOME}/log/SVNLOG_${PROJECT_NAME}
+        ${YANNKINS_HOME}/bin/cree_page
+        #dillo table.html
+    fi
+done
---
+++ bin/tache.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# this script is a task to be executed by yannkins
+# you need put in parameters the name of the task and the command
+# One line will be added in the log file ${LOGDIR}/${TACHE} :
+#     ${date};FAIL (in case of failure)
+#     ${date};OK   (in case of success)
+# The file ${LOGDIR}/${TACHE}_console will content the last console output
+
+
+IC="Yannkins"
+#DIR="/var/yannkins"
+#DIR="."
+DIR="${YANNKINS_HOME}"
+LOGDIR="${DIR}/log"
+
+function usage() {
+    printf "Execute a %s task\n" ${IC}
+    printf "Usage : %s <NOM_TACHE> <COMMANDE>\n" $0
+    exit 1
+}
+
+
+if [ $# -ne 2 ]; then
+    usage
+fi
+
+TACHE=$1
+COMMANDE="$2"
+#echo "Commande : ${COMMANDE}"
+DATE=$(date "+%d/%m/%Y %H:%M")
+RESULTAT=0
+
+if [ ! -d ${LOGDIR} ]; then
+    mkdir -p ${LOGDIR}
+    if [ $? -ne 0 ]; then
+        printf "Could not create the directory %s\n" ${LOGDIR}
+        printf "%s : task %s not completed\n" "${DATE}" ${TACHE}
+        exit 1
+    fi
+fi
+
+FICLOG="${LOGDIR}/${TACHE}"
+touch ${FICLOG}
+if [ $? -ne 0 ]; then
+    printf "Could not create or modify the file %s\n" ${FICLOG}
+    printf "%s : task %s not completed\n" "${DATE}" ${TACHE}
+    exit 1
+fi
+[ $(wc -c ${FICLOG} | cut -f 1 -d \ ) -eq 0 ] && echo "date;result" > ${FICLOG}
+
+
+FICCONSOLE="${LOGDIR}/${TACHE}_console"
+touch ${FICCONSOLE}
+if [ $? -ne 0 ]; then
+    printf "Could not create or modify the file %s\n" ${FICCONSOLE}
+    printf "%s : task %s not completed\n" "${DATE}" ${TACHE}
+    exit 1
+fi
+
+${COMMANDE} &> ${FICCONSOLE}
+RESULTAT=$?
+
+if [ ${RESULTAT} -eq 0 ]; then
+    RESULTAT="OK"
+else
+    RESULTAT="FAIL"
+fi
+
+printf "%s;%s\n" "${DATE}" ${RESULTAT} >> ${FICLOG}
+
